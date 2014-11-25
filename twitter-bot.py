c23a999568b4aea16290327613b3b69786f4384a
S Envis <smenvis@gmail.com>
2014-11-25 22:16:48 +0000

    Added additional bot methods

--- twitter-bot.py
+++ twitter-bot.py
@@ -14,7 +14,8 @@ __AUTHOR__ = 'smenvis'
 CONSUMER_KEY = ''
 CONSUMER_SECRET = ''
 
-SLEEP_TIME = 5
+SLEEP_TIME = 60
+
 
 class TBotInit:
     def main(self):
@@ -31,6 +32,7 @@ class TBotInit:
         self.cli()
 
     def cli(self):
+        '''Parse user input and call correct method'''
         command = raw_input('Enter command (type \'help\' for help): ')
         if command == 'start':
             self.tbot_start()
@@ -48,29 +50,30 @@ class TBotInit:
             self.cli()
 
     def tbot_setup(self):
+        '''Add Twitter account and keywords etc to database'''
         command = raw_input('Do you want to setup TweetBot (y/n)? ')
         if command == 'n':
             self.cli()
         elif command != 'y':
             print 'Error: You must enter \'y\' for yes or \'n\' no.'
-            self.tbot_config()
+            self.tbot_setup()
 
         print 'Please enter the following details to configure TweetBot:'
 
         username = ''
-        while username < 1:
+        while len(username) < 1:
             username = raw_input('Username: ')
 
-        access_tokens = self.authorise_twitter_app()
+        access_tokens = self.authorize_twitter_app()
 
         print 'Seperate Twitter Accounts and Keywords with \',\''
 
         accounts = ''
-        while accounts < 1:
+        while len(accounts) < 1:
             accounts = raw_input('Enter Twitter Accounts: ')
 
         keywords = ''
-        while keywords < 1:
+        while len(keywords) < 1:
             keywords = raw_input('Enter Keywords: ')
 
         conn = sqlite3.connect('twitter-bot.db')
@@ -95,19 +98,30 @@ class TBotInit:
         conn.commit()
 
         print 'Success: Twitter Account added to database.'
+
+        # Create database table to store followed twitter account details
+        try:
+            c.execute('''CREATE TABLE followed_accounts (
+                          username text,
+                          userid text,
+                          keyword text,
+                          date text)''')
+
+            print 'Success: Followed Accounts table created.'
+        except:
+            pass
+
+        conn.close()
         self.cli()
 
-    def authorise_twitter_app(self):
+    def authorize_twitter_app(self):
         auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
         auth_url = auth.get_authorization_url()
-
         print 'Please authorize: ' + auth_url
         verifier = raw_input('Enter PIN: ').strip()
         auth.get_access_token(verifier)
-
         #print "ACCESS_KEY = '%s'" % auth.access_token.key
         #print "ACCESS_SECRET = '%s'" % auth.access_token.secret
-
         return auth.access_token.key, auth.access_token.secret
 
     def remove_account(self):
@@ -131,12 +145,14 @@ class TBotInit:
 
         c.execute(sql)
         conn.commit()
+        conn.close()
 
         print 'Success: %s removed from TwitterBot' % username
 
         self.cli()
 
     def tbot_help(self):
+        '''TwitterBot help information'''
         print '\ntwitter-bot help information:\n'
         print '\thelp - prints this help information'
         print '\tstart - starts twitter-bot'
@@ -168,22 +184,28 @@ class TweetBot:
         self.access_token = self.account[1]
         self.access_token_secret = self.account[2]
         self.accounts = self.account[3]
-        #self.keywords = self.accounts[4]
+        self.keywords = self.account[4]
 
         auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
         auth.set_access_token(self.access_token, self.access_token_secret)
-
         self.api = tweepy.API(auth)
 
     def main(self):
+        '''Pull username for current user and start bot logic'''
         try:
             print 'Starting TweetBot for %s' % self.api.me().screen_name
+            print 'Account Stats: Following: %i Followers: %i'  % (
+                len(self.api.friends_ids()),
+                len(self.api.followers_ids()) )
         except:
             # TODO: Display actual returned error from Twitter
-            print 'Error: Unable to pull Twitter username.'
+            print 'Error: Unable to pull access Twitter API'
             return
 
-        print 'Picking random account'
+        self.decide_to_follow()
+        self.decide_to_unfollow()
+
+    def decide_to_follow(self):
         username = self.pick_random_account()
 
         print 'Pulling followers for %s' % username
@@ -195,44 +217,65 @@ class TweetBot:
             print 'Error: Unable to pull followers for %s' % username
             return
 
-        print 'Deciding to follow random user'
-        self.decide_to_follow(followers)
+        # Pick random user from followers
+        random_user = random.randrange(0,len(followers)) # -1
 
-        print 'Deciding to unfollow user'
-        self.decide_to_unfollow()
+        try:
+            username = self.api.get_user(followers[random_user]).screen_name
+        except:
+            print 'Error: Unable to get username from user id'
+            return
+
+        print 'Deciding to follow %s' % username
+
+        tweets = self.pull_tweets(username)
+
+        if tweets == 1:
+            return
+
+        name_kword = self.parse_tweets(tweets, username)
+        if name_kword == False:
+            return
+
+        self.follow_user(name_kword[0], followers[random_user], name_kword[1])
 
     def pick_random_account(self):
+        '''Pick random account from accounts in twitter-bot.db'''
+        print 'Picking random account'
         usernames = self.accounts.split(",")
         random_user = random.randrange(0,len(usernames)) # -1
         print 'Random account: %s' % usernames[random_user].strip()
         return usernames[random_user].strip()
 
-    def decide_to_follow(self, followers):
-        # pick random user from followers
-        random_user = random.randrange(0,len(followers)) # -1
-        print 'Deciding to follow %s' % followers[random_user]
+    def decide_to_unfollow(self):
+        print 'Deciding to unfollow random user'
+        # Pick random user from database
+        conn = sqlite3.connect('twitter-bot.db')
+        c = conn.cursor()
 
-        tweets = self.pull_tweets(followers[random_user])
-        if tweets == 1:
-            self.decide_to_follow(followers)
+        sql = "SELECT * FROM followed_accounts"
+        c.execute(sql)
 
-        if not self.parse_tweets(followers[random_user]):
+        accounts = c.fetchall()
+
+        random_account = random.randrange(0,len(accounts)) # -1
+
+        print 'Unfollowing user %s' % accounts[random_account][0]
+
+        # TODO: Check date user was followed is >= x
+        try:
+            self.api.destroy_friendship(accounts[random_account][0])
+        except:
+            print 'Error: Unable to unfollow user @%s' % accounts[random_account][0]
+            conn.close()
             return
 
-        # if so return user
-        # if not pick another user
-        # favourite tweet
-        # follow user
-        pass
+        sql = "DELETE FROM followed_accounts WHERE username ='%s'" % accounts[random_account][0]
 
-    def decide_to_unfollow(self):
-        # Check date user was followed
-        # If user was followed between 1 and two weeks ago (pick random date)
-        # - then unfollow
-        pass
+        c.execute(sql)
+        conn.commit()
 
-    def decide_to_favourite(self, tweets):
-        pass
+        conn.close()
 
     def pull_followers(self, username):
         try:
@@ -240,35 +283,70 @@ class TweetBot:
         except:
             return 1 # TODO: Return actual error
 
-    def pull_following(self, account):
-        # Pull users account is following
-        # Store in database
-        pass
+    def follow_user(self, username, userid, keyword):
+        conn = sqlite3.connect('twitter-bot.db')
+        c = conn.cursor()
 
-    def follow_user(self, username):
-        pass
+        sql = "SELECT * FROM followed_accounts"
+        c.execute(sql)
 
-    def unfollow_user(self, username):
-        pass
+        accounts = c.fetchall()
+        print 'Currently %i followed users in database' % len(accounts)
 
-    def favourite_tweet(self, tweet):
-        pass
 
-    def pull_tweets(self, userid):
-        print 'Pulling top 100 tweets for %s' % userid
+        sql = "SELECT * FROM followed_accounts WHERE username = '%s'" % username
+        c.execute(sql)
+
+        if len(c.fetchall()) > 0:
+            print '%s has already been followed on %s' % (username, account[3])
+            conn.close()
+            return
+
+        print 'Checking if %s has already been followed' % username
+
+        print 'Following user %s' % username
+
         try:
-            tweets = api.user_timeline(userid, count=100)
+            self.api.create_friendship(userid)
         except:
-            print 'Error: Unable to pull tweets for %s' % userid
+            print 'Error: Unable to follow user %s' % userid
+            conn.close()
+            return
+
+        # Add userid, username, keyword and time/date to database
+        sql = "INSERT INTO followed_accounts VALUES ('%s', '%s', '%s', '%s')" % (
+            username, userid, keyword, datetime.now() )
+
+        c.execute(sql)
+        conn.commit()
+
+        conn.close()
+
+    def pull_tweets(self, username):
+        print 'Pulling top 500 tweets for %s' % username
+        try:
+            tweets = self.api.user_timeline(username, count=500)
+        except:
+            print 'Error: Unable to pull tweets for %s' % username
             return 1
         
         return tweets
 
-    def parse_tweets(self, tweets):
-        # Check if keys words in tweets
-        # If so, return True
-        # Else return False
-        pass
+    def parse_tweets(self, tweets, username):
+        '''Parse tweets for keywords and return user if keyword found'''
+        keywords = self.keywords.split(",")
+        print 'Parsing %i tweets for %s' % (len(tweets), username)
+        for tweet in tweets:
+            for word in keywords:
+                # TODO: Also check for multi-word keywords in tweets
+                # TODO: Convert words to lower before comparing
+                if word.strip() in tweet.text.split():
+                    print '%s keyword found in tweet' % word.strip()
+                    print tweet.text
+                    return username, word
+
+        print 'No keywords found in tweets for %s' % username
+        return False
 
 
 if __name__ == '__main__':
@@ -277,4 +355,3 @@ if __name__ == '__main__':
         twitterbot.main()
 
 main()
-
